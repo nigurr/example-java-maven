95240159bc89aa21224d7b4b0fbb9489e7a170a4
Nitin Gurram <nigurr@microsoft.com>
2019-04-28 11:24:18 +0530

    Fix test reports

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-rm -f ./jacoco.xml
+rm -f ./*.xml
 
 echo "********** Building docker image ***********"
 docker build -t mvnsample ./
 
 echo "********** Copying Coverage report ***********"
 CID=$(docker run -d mvnsample)
+docker cp $CID:/target/surefire-reports/. ./
 docker cp $CID:/target/site/jacoco/jacoco.xml ./
 docker stop $CID
 
